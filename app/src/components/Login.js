140beca7d67a23fdddf6a94c6ac9007956bf4c04
nbabtsov@iastate.edu <nbabtsov@iastate.edu>
2020-10-23 08:36:11 -0500

    added bootstrap to login page, turned it into react form

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./Login.css";
 
-const Login = () => {
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const Styling = {width:"20rem", background:"#FFFFFF", border:"none", padding:"0.5rem"};
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
     return (
-        <div>
-            <div>
-                <input
-                    style={Styling}
-                    id='keyword'
-                    type='text'
-                    placeholder='Login'
-                />
-            </div>
-    <div>
-        <input
-            style={Styling}
-            id='keyword'
-            type='text'
-            placeholder='Password'
-        />
-    </div>
-            <button type="button">Login</button>
-
-
+        <div  className="Login">
+            <form onSubmit={handleSubmit}>
+                <FormGroup controlId="email" bsSize="large">
+                    {/*<ControlLabel>Email</ControlLabel>*/}
+                    <FormControl
+                        style={Styling}
+                        placeholder='Email'
+                        autoFocus
+                        type="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                </FormGroup>
+                <FormGroup controlId="password" bsSize="large">
+                    {/*<ControlLabel>Password</ControlLabel>*/}
+                    <FormControl
+                        style={Styling}
+                        placeholder="Password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        type="password"
+                    />
+                </FormGroup>
+                <Button block bsSize="large" disabled={!validateForm()} type="submit">
+                    Login
+                </Button>
+            </form>
         </div>
-
     );
 }
-
-export default Login;
