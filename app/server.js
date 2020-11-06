a20d5dfb2e9fb6917bbe859ba292c55a3837bf61
nbabtsov@iastate.edu <nbabtsov@iastate.edu>
2020-11-06 08:00:17 -0600

    successful connection to local DB

---
+++ app/server.js
@@ -0,0 +1,30 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+// app.get('/', (req, res) => {
+//     res.send('Hello World, from express');
+// });
+// app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+
+var mysql = require('mysql')
+var con = mysql.createConnection({
+    host: 'localhost', //'coms-319-t21.cs.iastate.edu',
+    user: 'root',
+    password: 'root',//'comsVM@319',
+    database: 'CareerCatcher'
+})
+
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM USER_TABLE", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        con.end(function(err) {
+            if (err) {
+                return console.log(err.message);
+            } else{
+                console.log("Close connection!");
+            }
+        });
+    });
+});
