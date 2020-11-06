cee2c8420d6d2bea8243d5edc0c480b844222fc1
nbabtsov@iastate.edu <nbabtsov@iastate.edu>
2020-11-06 09:17:46 -0600

    successful connection to server db

--- app/server.js
+++ app/server.js
@@ -1,16 +1,9 @@
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-// app.get('/', (req, res) => {
-//     res.send('Hello World, from express');
-// });
-// app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
 
 var mysql = require('mysql')
 var con = mysql.createConnection({
-    host: 'localhost', //'coms-319-t21.cs.iastate.edu',
-    user: 'root',
-    password: 'root',//'comsVM@319',
+    host: 'coms-319-t21.cs.iastate.edu', 
+    user: 'admin',
+    password: 'password',
     database: 'CareerCatcher'
 })
 
@@ -19,13 +12,7 @@ con.connect(function(err) {
     con.query("SELECT * FROM USER_TABLE", function (err, result, fields) {
         if (err) throw err;
         console.log(result);
-        // con.end(function(err) {
-        //     if (err) {
-        //         return console.log(err.message);
-        //     } else{
-        //         console.log("Close connection!");
-        //     }
-        // });
+
     });
     con.query("SELECT * FROM JOB_TABLE", function (err, result, fields) {
         if (err) throw err;
--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {Component, useState} from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Navigation from './components/Navigation'
 import Homepage from './components/Homepage'
@@ -6,28 +6,76 @@ import Joblistpage from './components/Joblistpage'
 import Login from './components/Login'
 import Signup from './components/Signup'
 
-
-const App = () => {
-	const [username, setUsername] = useState("");
-
-	function handleUser(newValue){
-		setUsername(newValue);
+class App extends Component {
+constructor(props){
+	super(props);
+	this.state = {apiResponse : "", username:""};
+}
+	callAPI(){
+		fetch('http://localhost:9000/testAPI')
+			.then(res => res.text())
+			.then(res => this.setState({ apiResponse: res }));
 	}
-
+	componentDidMount() {
+	this.callAPI();
+	}
+	render(){
 	return(
 		<BrowserRouter>
 			<div>
-				<Navigation username={username}/>
+				<Navigation username={this.state.username}/>
 				<Switch>
 					<Route path="/joblist" component={Joblistpage}/>
-					<Route path="/Login" render={(props) => (<Login username={username} handleUser={handleUser}/>)} />
+					<Route path="/Login" render={(props) => (<Login username={this.state.username} />)} />
 					<Route path="/Signup" component={Signup}/>
 					<Route exact path="/" component={Homepage}/>
 
 				</Switch>
+				<div>
+					<p className="App-intro">{this.state.apiResponse}</p>
+				</div>
+
 			</div>
 		</BrowserRouter>
-	);
+		);
+	}
 }
+
+
+// const App = () => {
+// 	const [username, setUsername] = useState("");
+// 	const [apiResponse, setApiResponse] = useState("");
+// 	function handleUser(newValue){
+// 		setUsername(newValue);
+// 	}
+//
+// 	function callAPI(){
+// 		fetch('http://localhost:9000/testAPI')
+// 			.then(res => res.text())
+// 			.then(res => setState({ apiResponse: res }));
+// 	}
+// 	function ComponentDidMount(){
+// 		callAPI();
+// 	}
+//
+// 	return(
+// 		<BrowserRouter>
+// 			<div>
+// 				<Navigation username={username}/>
+// 				<Switch>
+// 					<Route path="/joblist" component={Joblistpage}/>
+// 					<Route path="/Login" render={(props) => (<Login username={username} handleUser={handleUser}/>)} />
+// 					<Route path="/Signup" component={Signup}/>
+// 					<Route exact path="/" component={Homepage}/>
+//
+// 				</Switch>
+// 				<div>
+// 					<p className="App-intro">{apiResponse}</p>
+// 				</div>
+//
+// 			</div>
+// 		</BrowserRouter>
+// 	);
+// }
 
 export default App;
