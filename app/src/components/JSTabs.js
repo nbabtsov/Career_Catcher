59fd7eec9734bb38b09e322df62ef4c993e23b4a
Kyle Riggs <kjriggs@iastate.edu>
2020-11-18 00:38:32 -0600

    Added more content to tabs and tweaked design

--- app/src/components/JSTabs.js
+++ app/src/components/JSTabs.js
@@ -1,40 +1,6 @@
 import React from 'react';
 import './CSSTabs.css';
-
-const JSTabs = props => {
-    return(
-      <div>
-       <Tabs>
-         <Tab label="About Us">
-           <div>
-             <p>We just tryna get that A honestly</p>
-           </div>
-         </Tab>
-         <Tab label="Mission Statement">
-           <div>
-             <p>Tab 2 content</p>
-           </div>
-         </Tab>
-         <Tab label="Contact Us">
-           <div>
-             <p>Tab 3 content</p>
-           </div>
-         </Tab>
-         <Tab label="Tab 4">
-           <div>
-             <p>Tab 4 content</p>
-           </div>
-         </Tab>
-         <Tab label="Tab 5">
-           <div>
-             <p>Tab 5 content</p>
-           </div>
-         </Tab>
-       </Tabs>
-      </div>
-      
-    )
-  }
+import styled from 'styled-components'
 
 class Tabs extends React.Component {
   state = {
@@ -78,6 +44,72 @@ const Tab = props =>{
     <React.Fragment>
       {props.children}
     </React.Fragment>
+  )
+}
+
+const Text = styled.p`
+  font-size: 1.2em;
+  color: #9400D3;
+  text-align: center;
+  margin-right: 500px;
+  margin-left: 500px;
+`
+
+const JSTabs = props => {
+  return(
+    <div>
+     <Tabs>
+       <Tab label="About Us">
+         <Text>
+         <div>
+           <p>In essence, we are a just a small group of Computer Science 319 students
+             teamed up to complete a web scraping application. However, the cohesiveness
+             and tact this group portrays is not done justice by that simple definition.
+             The team is comprised of the brilliant and talented minds of Logan Anderson,
+             Ryan Patterson, Nika Babtsov, and Kyle Riggs. Together we have toiled and
+             strained to bring you the best job finidng program a school project can give!
+           </p>
+         </div>
+         </Text>
+       </Tab>
+       <Tab label="Mission Statement">
+         <Text>
+         <div>
+           <p>We have made it our goal to produce an efficient and easy-to-use job
+             searching website that users will benefit from immensly. Extreme care has 
+             been taken to present you with a sleek and attractive website that will aid 
+             you in finding a phenonmenal job fit to your very own parameters. We have
+             allowed you, the user, to search for jobs by Title, Employer, Location, or
+             even Description! We not only gaurantee you will find a great job fitting your
+             ideal wants, but also we gaurantee you will greatly enjoy doing so. So, we thank
+             you for your use of our website and we congratulate you on the perfect job you
+             are about to find yourself working for!
+           </p>
+         </div>
+         </Text>
+       </Tab>
+       <Tab label="Contact Us">
+         <Text>
+         <div>
+           <p>Please feel free to contact us with any questions or concerns at 
+             shibbir@iastate.edu and we look foward to hearing from you!
+           </p>
+         </div>
+         </Text>
+       </Tab>
+       <Tab label="Tab 4">
+         <div>
+           <p>Filler Text</p>
+         </div>
+       </Tab>
+       <Tab label="Tab 5">
+         <div>
+           <p>Filler Text</p>
+         </div>
+       </Tab>
+     </Tabs>
+    </div>
+    
   )
 }
 
