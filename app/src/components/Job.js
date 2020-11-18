e2d632c7b3425efd4579cf58a19579b8f2b4a76e
Ryan Patterson <rjp1@iastate.edu>
2020-11-18 02:00:24 -0600

    improved joblist design

--- app/src/components/Job.js
+++ app/src/components/Job.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import './Job.css'
+import styled from 'styled-components'
 
 const Job = (job, index) => {
 	return(
 		<div key={index} className="Job_Div">
 			<section   className="Job_Section">
-		 <p>{job.jobTitle}</p>
-		 <p>{job.jobGiver}</p>
-		 <p>{job.location}</p>
-		 <p>${job.payment}</p>
-		 <p>{job.description}</p>
-		 <button className={"./Job.css.Save_Job"}>Save Job</button>
+		<div className="job_Title"><p>{job.jobTitle}</p></div> 
+		<div className="job_Giver"><p>{job.jobGiver}</p></div> 
+		<div className="job_Location"><p>{job.location}</p></div>
+		<div className="job_Payment"><p>${job.payment}</p></div> 
+		<div className="job_Description"><p>{job.description}</p></div>
+		 <Button  type="button"><span>Save Job</span></Button>
 			</section>
 			<br/>
 			
 		</div>
 	)
 }
+const Button = styled.button`
+  display: block;
+  width: 60%;
+  padding: 12px 0;
+  font-family: inherit;
+  font-size: 14px;
+  font-weight: 700;
+  color: #fff;
+  background-color: #e5195f;
+  border: 0;
+  border-radius: 35px;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, .08);
+  cursor: pointer;
+  transition: all .25s cubic-bezier(.02, .01, .47, 1);
+	margin-left:20%;
+  &:hover {
+    box-shadow: 0 15px 15px rgba(0, 0, 0, .16);
+    transform: translate(0, -2px);
+  }
+`
 
 export default Job;
